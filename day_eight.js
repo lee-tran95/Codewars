1d4322f44afcbe93005066fb251270c9cb8fec30
Lee Tran <lee.tran95@yahoo.com>
2022-04-17 20:25:45 -0700

    push to an array a number

--- day_eight.js
+++ day_eight.js
@@ -4,3 +4,14 @@
 function hero(bullets, dragons){
     return (bullets/dragons) >= 2 ? true : false
   }
+
+//   Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+const reverseSeq = n => {
+    let array = [];
+    for (let i = 0; i < n; i++){
+      array.push(n-i);
+    }
+    return array;
+  };
