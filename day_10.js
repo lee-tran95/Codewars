351602b25690112e5d3e883cbb42c5609521ba3d
Lee Tran <lee.tran95@yahoo.com>
2022-04-19 20:37:55 -0700

    Find the sum of all the numbers between two given numbers

--- day_10.js
+++ day_10.js
@@ -44,14 +44,27 @@ function sum (numbers) {
     }
   };
 
-// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+//   Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
 
-// Note: a and b are not ordered!
+//   Note: a and b are not ordered!
+  
+//   Examples (a, b) --> output (explanation)
+//   (1, 0) --> 1 (1 + 0 = 1)
+//   (1, 2) --> 3 (1 + 2 = 3)
+//   (0, 1) --> 1 (0 + 1 = 1)
+//   (1, 1) --> 1 (1 since both are same)
+//   (-1, 0) --> -1 (-1 + 0 = -1)
+//   (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
 
-// Examples (a, b) --> output (explanation)
-// (1, 0) --> 1 (1 + 0 = 1)
-// (1, 2) --> 3 (1 + 2 = 3)
-// (0, 1) --> 1 (0 + 1 = 1)
-// (1, 1) --> 1 (1 since both are same)
-// (-1, 0) --> -1 (-1 + 0 = -1)
-// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+function getSum( a,b )
+{
+   let min = Math.min(a,b)
+   let max = Math.max(a,b)
+   let sum = 0
+   for (let i = min; min <= max; min ++)
+     {
+       sum += min;
+     }
+  return sum
+}
+// (max - min + 1) * (min + max) / 2 is the math way to solve this problem
