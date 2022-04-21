03a3231ac03497317d592eda86b6d1dea27df4c1
Lee Tran <lee.tran95@yahoo.com>
2022-04-20 20:31:08 -0700

    Square each digit in a given number then return them concatanated

--- day_11.js
+++ day_11.js
@@ -51,3 +51,17 @@ function numberToString(num) {
 function numberToString(num) {
     return num.toString();
   }
+
+// Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+// Note: The function accepts an integer and returns an integer
+
+function squareDigits(num){
+    num = String(num);
+    num = num.split('');
+    num = num.map(item => Math.pow(item,2));
+    num = num.join('');
+    return Number(num)
+  }
