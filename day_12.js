f81bb383b6c646fe1aff3c546ed8ca33aff2406b
Lee Tran <lee.tran95@yahoo.com>
2022-04-21 18:51:51 -0700

    Capitalize every word in a string and delete the first and last letter of a str

---
+++ day_12.js
@@ -0,0 +1,40 @@
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+// Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+// Example:
+// n= 5, m=5: 25
+// n=-5, m=5:  0
+
+function paperwork(n, m) {
+    return n < 0 || m < 0 ? 0 : n*m
+  }
+
+//   Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+
+//   Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+//   Example:
+
+//   Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+//   Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+String.prototype.toJadenCase = function() {
+    return this.split(' ').map(item => item[0].toUpperCase() + item.slice(1)).join(' ')
+  };
+
+// Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+// Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+function lovefunc(flower1, flower2){
+    return (flower1 +flower2)%2 == 0 ? false:true
+  }
+
+//   It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. 
+//   You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+function removeChar(str){
+    return str.slice(1, -1)
+  };
+  
