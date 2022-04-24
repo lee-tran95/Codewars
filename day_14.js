60ffcc261f416faa2a3b85d91f6f5c9cbbd3f849
Lee Tran <lee.tran95@yahoo.com>
2022-04-23 23:39:45 -0700

    Filter array to only include integers using Number.isInteger()

--- day_14.js
+++ day_14.js
@@ -7,5 +7,5 @@
 // filter_list([1,2,'aasf','1','123',123]) 
 
 function filter_list(l) {
-    return l.filter(item => typeof item === 'number')
+    return l.filter(item => Number.isInteger(item))
   }
