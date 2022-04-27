8d470e52275739b0dfd51fd04eb331be204d91c7
Lee Tran <lee.tran95@yahoo.com>
2022-04-26 23:51:15 -0700

    Return an array with the matching DNA pair from the given string

--- day_16.js
+++ day_16.js
@@ -58,4 +58,52 @@ function addBinary(a,b) {
       binary = (number%2) + binary;
     }
     return binary
-  }S
+  }
+
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+// If you want to know more: http://en.wikipedia.org/wiki/DNA
+
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+// More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+
+// Example: (input --> output)
+
+// "ATTGC" --> "TAACG"
+// "GTAT" --> "CATA"
+// dnaStrand []        `shouldBe` []
+// dnaStrand [A,T,G,C] `shouldBe` [T,A,C,G]
+// dnaStrand [G,T,A,T] `shouldBe` [C,A,T,A]
+// dnaStrand [A,A,A,A] `shouldBe` [T,T,T,T]
+
+function DNAStrand(dna){
+  
+    if(!dna.length){
+      return dna
+    }
+    else{
+      dna = dna.split('')
+      return dna.map(item =>{
+        if(item =='A'){
+          return 'T'
+        }else if(item == 'T'){
+          return 'A'
+        }else if(item == 'C'){
+          return 'G'
+        }else if(item == 'G'){
+          return 'C'
+        }
+      }).join('')
+    }
+  }
+
+function DNAStrand(dna){
+let hashmap = new Map();
+hashmap.set('A','T')
+hashmap.set('T','A')
+hashmap.set('C','G')
+hashmap.set('G','C')
+dna = dna.split('');
+return dna.map(item => hashmap.get(item)).join('')
+}
