16b732c88de195ac822040d0143e9a65595b3b5c
Lee Tran <lee.tran95@yahoo.com>
2022-04-28 00:01:41 -0700

    Determine if the consecutive power of each digits in a number added together would equal the number

--- day_17.js
+++ day_17.js
@@ -28,3 +28,43 @@ function even_or_odd(number) {
       return "Odd"
     }
   }
+
+// The number 89 is the first integer with more than one digit that fulfills the property partially introduced in the title of this kata. What's the use of saying "Eureka"? Because this sum gives the same number.
+
+// In effect: 89 = 8^1 + 9^2
+
+// The next number in having this property is 135.
+
+// See this property again: 135 = 1^1 + 3^2 + 5^3
+
+// We need a function to collect these numbers, that may receive two integers a, b that defines the range [a, b] (inclusive) and outputs a list of the sorted numbers in the range that fulfills the property described above.
+
+// Let's see some cases:
+
+// sumDigPow(1, 10) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// sumDigPow(1, 100) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
+// If there are no numbers of this kind in the range [a, b] the function should output an empty list.
+
+// sumDigPow(90, 100) == []
+// Enjoy it!!
+
+function sumDigPow(a, b) {
+    let array = []
+    //for loop to loop through the range a-b 
+    for(let i = a; i < b; i ++){
+      //convert to string so that we can loop through big numbers in order to apply the power rule
+      i = i.toString()
+      let sum = 0;
+      //loop through each number in a 2 digit or more number
+      for (let j = 0; j < i.length; j++){
+        //sum the power of each digit to determine if it matches the original number
+        sum += Math.pow(i[j],j+1)
+      }
+      if( i == sum){
+        array.push(Number(i))
+      }
+    }
+    return array;
+  }
+  
