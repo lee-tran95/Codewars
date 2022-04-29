69437eba0c0b9a536385b4d80255eae306e79ae9
Lee Tran <lee.tran95@yahoo.com>
2022-04-28 21:59:03 -0700

    Function to return the expanded form of a number

--- day_18.js
+++ day_18.js
@@ -34,3 +34,56 @@ function deleteNth(arr,n){
       return cache[item] <= n
     })
   }
+
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+  
+function isTriangle(a,b,c)
+{
+  if(a+b <= c || a+c <= b || b+c <= a)
+   return false;
+  else{
+   return true;
+  }
+}
+
+// Write Number in Expanded Form
+// You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+// expandedForm(12); // Should return '10 + 2'
+// expandedForm(42); // Should return '40 + 2'
+// expandedForm(70304); // Should return '70000 + 300 + 4'
+// NOTE: All numbers will be whole numbers greater than 0.
+
+// If you liked this kata, check out part 2!!
+
+function expandedForm(num) {
+    let expanded = '';
+    num = num.toString();
+    let length = num.length;
+    for(let i = 0; i < num.length;i++){
+      if(num[i] == 0){
+        length--
+      }else if(num[i] != 0 && i != 0){
+        expanded += ' + '
+        expanded += num[i] + '0'.repeat(length-1);
+        length--
+      }
+        else if(num[i] != 0){
+        expanded += num[i] + '0'.repeat(length-1);
+        length--
+        }
+    }
+    return expanded
+  }
+
+  function expandedForm(num) {
+    num = num.toString();
+    num = num.split('');
+    num = num.map((item,index) => {
+      return item + '0'.repeat(num.length - index - 1)
+    });
+    num = num.filter(item => item != 0)
+    return num.join(' + ')
+  }
