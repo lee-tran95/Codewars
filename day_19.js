49ae7e656bcdda34def06ec73682efd56c16fc12
Lee Tran <lee.tran95@yahoo.com>
2022-04-29 22:05:39 -0700

    Function to sum all of the array elements and then determine if the sum is odd or even

--- day_19.js
+++ day_19.js
@@ -38,3 +38,25 @@ function removeEveryOther(arr){
       return index%2 ==0
     })
   }
+
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+// Examples:
+// Input: [0]
+// Output: "even"
+
+// Input: [0, 1, 4]
+// Output: "odd"
+
+// Input: [0, -1, -5]
+// Output: "even"
+
+function oddOrEven(array) {
+    return array.reduce((sum,current)=>{
+      return sum += current
+    },0)%2 === 0 ? "even":"odd"
+ }
