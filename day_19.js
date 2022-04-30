aa23733c3b7a816e512ff7330c173f6395fe2693
Lee Tran <lee.tran95@yahoo.com>
2022-04-29 22:01:37 -0700

    Function to remove every other elements in an array

--- day_19.js
+++ day_19.js
@@ -25,3 +25,16 @@ function isIsogram(str){
     return new Set(str.toUpperCase()).size == str.length;
   }
 
+// Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+// Example:
+
+// ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+// None of the arrays will be empty, so you don't have to worry about that!
+
+function removeEveryOther(arr){
+    return arr.filter((item,index) =>{
+      return index%2 ==0
+    })
+  }
