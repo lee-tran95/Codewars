c435181f10bf1895a1614dfda121fb63ca3bc29c
Lee Tran <lee.tran95@yahoo.com>
2022-05-01 00:04:46 -0700

    Function to sort words in a string that contains a number in each word

---
+++ day_20.js
@@ -0,0 +1,34 @@
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples:
+
+// number([]) // => []
+// number(["a", "b", "c"]) // => ["1: a", "2: b", "3: c"]
+var number=function(array){
+    for(let i = 0; i < array.length; i++){
+      array[i] = `${i+1}: ${array[i]}`
+    }
+    return array
+  }
+
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+// Examples
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
+function order(words){
+    words = words.split(' ');
+    return words.sort((a,b)=> {
+      return a.match(/\d/) - b.match(/\d/)
+    }).join(' ')
+  }
