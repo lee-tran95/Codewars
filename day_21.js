a0a2a4b710333095c832f4ec152b77368f3b7a60
Lee Tran <lee.tran95@yahoo.com>
2022-05-01 23:20:42 -0700

    Function to find the alphabet index position of each char in a string

---
+++ day_21.js
@@ -0,0 +1,25 @@
+// Welcome.
+
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+function alphabetPosition(text) {
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    let position =[];
+    text = text.toLowerCase();
+    text = text.split('');
+    for(let i = 0; i < text.length; i++){
+      if(alphabet.indexOf(text[i]) >= 0){
+   
+        position.push(alphabet.indexOf(text[i])+1)
+      }
+    }
+    return position.join(' ');
+  }
