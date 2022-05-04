5f0771d38694feb90743d7a99cca8d9731e1a908
Lee Tran <lee.tran95@yahoo.com>
2022-05-04 14:16:40 -0700

    Function to return an array in unique order

--- day_24.js
+++ day_24.js
@@ -46,3 +46,25 @@ function getCount(str) {
     vowelsCount = str.match(/[aeiou]/gi);
     return vowelsCount ? vowelsCount.length:0;
   }
+
+//   Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+//   For example:
+  
+//   uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+//   uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+//   uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+var uniqueInOrder=function(iterable){
+    let unique = [];
+    for(let i = 0; i < iterable.length; i++){
+      if(i == 0){
+        unique.push(iterable[i])
+      }else{
+        if(iterable[i] != iterable[i-1]){
+          unique.push(iterable[i])
+        }
+      }
+    }
+    return unique
+  }
