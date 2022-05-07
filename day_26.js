d5bdc6058a5b4d260329955695429b67d3115c1c
Lee Tran <lee.tran95@yahoo.com>
2022-05-06 21:42:32 -0700

    Function to convert binary number into an integer

---
+++ day_26.js
@@ -0,0 +1,50 @@
+// The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+
+// To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+// Input
+// Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+
+// Output
+// Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+
+// Example
+// input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+// output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+
+function openOrSenior(data){
+    let membership = []
+    for(let i = 0; i < data.length; i++){
+      if(data[i][0] >= 55 && data[i][1] > 7){
+        membership.push('Senior')
+      }else{
+        membership.push('Open')
+      }
+    }
+    return membership
+  }
+
+// Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+// Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+// Examples:
+
+// Testing: [0, 0, 0, 1] ==> 1
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 0, 1] ==> 5
+// Testing: [1, 0, 0, 1] ==> 9
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 1, 0] ==> 6
+// Testing: [1, 1, 1, 1] ==> 15
+// Testing: [1, 0, 1, 1] ==> 11
+// However, the arrays can have varying lengths, not just limited to 4.
+
+const binaryArrayToNumber = arr => {
+    let sum = 0;
+    for(let i = 0; i <arr.length; i++){
+      sum = sum + arr[i]*(2**(arr.length-1-i))
+    }
+    return sum;
+  };
+
