2d2fdb03ac510284ef760e9a4cf3f5ff5963478f
Lee Tran <lee.tran95@yahoo.com>
2022-05-09 15:24:22 -0700

    Assign points to words in an array given alphabet position of each letters and find the highest

--- day_29.js
+++ day_29.js
@@ -93,3 +93,33 @@ function race(v1, v2, g) {
    if(v1 > v2) return null
    return [Math.floor(time),Math.floor(time*60%60),Math.floor(time*3600%60)]
  }
+
+//  Given a string of words, you need to find the highest scoring word.
+
+// Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+// You need to return the highest scoring word as a string.
+
+// If two words score the same, return the word that appears earliest in the original string.
+
+// All letters will be lowercase and all inputs will be valid.
+
+function high(x){
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    x = x.split(' ');
+    let sumAll = 0;
+    let sumCurrent = 0;
+    let highest = 0;
+    for(let i = 0; i < x.length; i++){
+      for(let j = 0; j < x[i].length; j++){
+        sumCurrent += alphabet.indexOf(x[i][j])+1
+        console.log(sumCurrent)
+      }
+      if(sumCurrent > sumAll){
+        sumAll = sumCurrent;
+        highest = x[i];
+      }
+      sumCurrent = 0;
+    }
+    return highest;
+  }
