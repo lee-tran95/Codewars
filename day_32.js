e089427e23c29621628aef914ae2f67c01c2b9c1
Lee Tran <lee.tran95@yahoo.com>
2022-05-13 00:10:17 -0700

    Find the unique number within an array of number

--- day_32.js
+++ day_32.js
@@ -15,3 +15,31 @@ var max = function(list){
     list.sort((a,b) => b-a)
     return list[0];
 }
+
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains at least 3 numbers.
+
+// The tests contain some very huge arrays, so think about performance.
+
+// This is the first kata in series:
+
+// Find the unique number (this kata)
+// Find the unique string
+// Find The Unique
+
+function findUniq(arr) {
+    arr = arr.sort((a,b) => a-b)
+    console.log(arr)
+      if(arr[0] === arr[1] && arr[0] !== arr[arr.length-1]){
+        return arr[arr.length-1]
+      }else{
+        return arr[0]
+      }
+  }
+
+  function findUniq(arr) {
+    return arr.find(e => arr.indexOf(e) === arr.lastIndexOf(e))
+    }
