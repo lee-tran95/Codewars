043534bf01e3af2b234efc8b33c803265f5b945f
Lee Tran <lee.tran95@yahoo.com>
2022-05-16 14:30:27 -0700

    Function to take in a string and convert it to title case

--- day_35.js
+++ day_35.js
@@ -24,15 +24,49 @@
 // Negative numbers and duplicate numbers can and will appear.
 
 function sumPairs(ints, s) {
-    let first = 0;
-    let second = 0;
-    for(let i = 0; i < ints.length; i++){
-      let dif = s - ints[i];
-      if(ints.indexOf(dif) >= 0){
-        first = ints[i]
-        second = ints[ints.indexOf(dif)]
-        return [first, second];
-      }
+  let seen = {};
+  for(let i = 0; i < ints.length; i++){
+    let dif = s - ints[i];
+    if(seen[dif] !== undefined){
+      return [dif, ints[i]]
     }
-    return undefined
+    if(seen[dif] === undefined){
+      seen[ints[i]] = i
+    }
   }
+  return undefined;
+}
+
+// A string is considered to be in title case if each word in the string is either (a) capitalised (that is, only the first letter of the word is in upper case) or (b) considered to be an exception and put entirely into lower case unless it is the first word, which is always capitalised.
+
+// Write a function that will convert a string into title case, given an optional list of exceptions (minor words). The list of minor words will be given as a string with each word separated by a space. Your function should ignore the case of the minor words string -- it should behave in the same way even if the case of the minor word string is changed.
+
+// ###Arguments (Haskell)
+
+// First argument: space-delimited list of minor words that must always be lowercase except for the first word in the string.
+// Second argument: the original string to be converted.
+// ###Arguments (Other languages)
+
+// First argument (required): the original string to be converted.
+// Second argument (optional): space-delimited list of minor words that must always be lowercase except for the first word in the string. The JavaScript/CoffeeScript tests will pass undefined when this argument is unused.
+// ###Example
+
+// titleCase('a clash of KINGS', 'a an the of') // should return: 'A Clash of Kings'
+// titleCase('THE WIND IN THE WILLOWS', 'The In') // should return: 'The Wind in the Willows'
+// titleCase('the quick brown fox') // should return: 'The Quick Brown Fox'
+
+function titleCase(title, minorWords) {
+  if(title.length === 0){
+    return ''
+  }
+  let minors = minorWords? minorWords.toLowerCase().split(' '):[];
+  let words = title
+    .toLowerCase()
+    .split(' ')
+  return words.map((elem, index) =>{
+    if(index !== 0 && minors.indexOf(elem) >= 0){
+      return elem
+    }
+    return elem.charAt(0).toUpperCase() + elem.splice(1)
+  })
+}
